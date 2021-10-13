1a0a5e99ae1758feec67acf418696cd2bcc1afbd
tongyan <tongyan@haomo.ai>
2021-10-13 13:41:59 -0400

    update ScanUtils, complete Controller

--- src/Controller.java
+++ src/Controller.java
@@ -6,5 +6,43 @@
 public class Controller {
     public static void run() {
         // TODO:
+        System.out.println("Welcome to card games!");
+        while (true) {
+            System.out.println("Enter 'b' for playing black jack. " +
+                    "Enter 't' for playing trianta ena. " +
+                    "Enter 'e' for exit: ");
+            String enter = ScanUtils.scanString();
+            while (!enter.equalsIgnoreCase("b") &&
+                    !enter.equalsIgnoreCase("t") &&
+                    !enter.equalsIgnoreCase("e")) {
+                System.out.println("invalid input, please enter 'b' for black jack, " +
+                        "'t' for trianta ena, or 'e' for exit.");
+                enter = ScanUtils.scanString();
+            }
+            if (enter.equalsIgnoreCase("e")) {
+                System.out.println("Thank you for your playing!");
+                return;
+            }
+            if (enter.equalsIgnoreCase("b")) {
+                BJGame bjGame = createBJGame();
+                bjGame.play();
+            } else if (enter.equalsIgnoreCase("t")) {
+                TEGame teGame = createTEGame();
+                teGame.play();
+            }
+        }
+    }
+
+    private static BJGame createBJGame() {
+        // BJGame only allows 2 people to play
+        BJGame bjGame = new BJGame(2);
+        return bjGame;
+    }
+
+    private static TEGame createTEGame() {
+        System.out.println("Enter number of players going to attend:");
+        int playerNum = ScanUtils.scanInt(2, Integer.MAX_VALUE);
+        TEGame teGame = new TEGame(playerNum);
+        return teGame;
     }
 }
--- src/ScanUtils.java
+++ src/ScanUtils.java
@@ -12,9 +12,24 @@ public class ScanUtils {
         while (true) {
             if (sc.hasNext()) {
                 int num = sc.nextInt();
-                if (num < low || num > high) {
-                    System.out.printf("invalid. please input number from %d--%d\n", low, high);
-                    continue;
+                if (low == Integer.MIN_VALUE && high == Integer.MAX_VALUE) {
+                    return num;
+                }
+                if (low == Integer.MIN_VALUE) {
+                    if (num > high) {
+                        System.out.printf("invalid. please input number <= %d:\n", high);
+                        continue;
+                    }
+                } else if (high == Integer.MAX_VALUE) {
+                    if (num < low) {
+                        System.out.printf("invalid. please input number >= %d:\n", low);
+                        continue;
+                    }
+                } else {
+                    if (num < low || num > high) {
+                        System.out.printf("invalid. please input number from %d--%d\n", low, high);
+                        continue;
+                    }
                 }
                 return num;
             }
@@ -25,9 +40,24 @@ public class ScanUtils {
         while (true) {
             if (sc.hasNext()) {
                 double num = sc.nextDouble();
-                if (num < low || num > high) {
-                    System.out.printf("invalid. please input number from %f--%f\n", low, high);
-                    continue;
+                if (low == Double.MIN_VALUE && high == Double.MAX_VALUE) {
+                    return num;
+                }
+                if (low == Double.MIN_VALUE) {
+                    if (num > high) {
+                        System.out.printf("invalid. please input number <= %f:\n", high);
+                        continue;
+                    }
+                } else if (high == Double.MAX_VALUE) {
+                    if (num < low) {
+                        System.out.printf("invalid. please input number >= %f:\n", low);
+                        continue;
+                    }
+                } else {
+                    if (num < low || num > high) {
+                        System.out.printf("invalid. please input number from %f--%f\n", low, high);
+                        continue;
+                    }
                 }
                 return num;
             }
