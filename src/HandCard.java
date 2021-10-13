fc6816ae064c36ae3b635dc2796f2ef80764241c
Friende <youpeng@bu.edu>
2021-10-13 11:52:05 -0400

    implement HandCard

--- src/HandCard.java
+++ src/HandCard.java
@@ -1,31 +1,60 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * @className: HandCard
  * @description: class of player's hand cards
  * @author: You Peng
  **/
+
+
 public class HandCard {
     private List<Card> handCardList;
-    private int totalPoints;
     private boolean isBusted;
-    private int winStatus;  // -1: lose, 0: tie, 1: win
+    private int totalPoints;
+    private int winStatus;  // other: none, -1: lose, 0: tie, 1: win
+    private final int MAX_POINT; // For BlackJack it's 21, for Trianta Ena it's 31.
 
-    public HandCard() {
+    /**
+     *
+     * @param max_point
+     */
+    public HandCard(int max_point) {
+        if (max_point < 1)  throw new IllegalArgumentException("Error: Max point is invalid!");
+
+        this.MAX_POINT      = max_point;
+        this.handCardList   = new ArrayList<>();
+        this.totalPoints    = 0;
+        this.isBusted       = false;
+        this.winStatus      = Integer.MIN_VALUE;
     }
 
     public void addCard(Card card) {
         // TODO:
+        if (card != null) {
+            handCardList.add(card);
+            totalPoints += card.getFaceValue();
+        }
     }
 
-    public Card removeCard() {
-        // TODO:
-        return null;
+    public Card randomRemoveCard() {
+        // TODO: rename the function to randomRemoveCard
+        // create instance of Random class
+        Random rand = new Random();
+        // Generate random integers in range 0 to list size
+        int randId = rand.nextInt(handCardList.size());
+        return handCardList.remove(randId);
     }
 
+    /**
+     *
+     * @param   card
+     * @return  The card that is removed. "null" if the card doesn't exist.
+     */
     public Card removeCard(Card card) {
         // TODO:
-        return null;
+        return handCardList.remove(card) ? card : null;
     }
 
     public List<Card> getHandCardList() {
