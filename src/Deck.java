4a8529d3f81ab8f0c7f4b79ca730a40f84763028
tongyan <tongyan@haomo.ai>
2021-10-13 13:20:34 -0400

    complete ScanUtils and Deck class. add isEmpty() method in Deck class

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * @className: Deck
@@ -7,15 +9,61 @@ import java.util.List;
  **/
 public class Deck {
     private List<Card> cards;
-    private int randomSeed;
-    private int rightBound;
+    private long randomSeed;
+    private int rightBound; // choose index from [0, rightBound], both inclusive
+
+    public Deck() {
+        this.cards = createCard();
+        this.randomSeed = System.nanoTime();
+        this.rightBound = this.cards.size() - 1;
+    }
 
     public Card popCard() {
-        // TODO:
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        // use random number to select pop card index
+        Random random = new Random(randomSeed);
+        int index = random.nextInt(this.rightBound + 1);
+        Card chosenCard = this.cards.get(index);
+        // after pop, we should swap chosen card with right bound card, and move right bound left
+        swap(this.cards, index, rightBound--);
+        return chosenCard;
     }
 
     public void shuffle() {
-        // TODO:
+        // reset random seed and right bound
+        this.randomSeed = System.nanoTime();
+        this.rightBound = this.cards.size() - 1;
+    }
+
+    // to see whether there is valid card to pop
+    public boolean isEmpty() {
+        return this.rightBound < 0;
+    }
+
+    private void swap(List<Card> cards, int i, int j) {
+        Card c = cards.get(i);
+        cards.set(i, cards.get(j));
+        cards.set(j, c);
+    }
+
+    private List<Card> createCard() {
+        List<Card> res = new ArrayList<>();
+        // 2-10, each num has 4 cards
+        for (int i = 2; i <= 10; i++) {
+            for (int j = 0; j < 4; j++) {
+                res.add(new Card(String.valueOf(i)));
+            }
+        }
+        // A, J, Q, K, each has 4 cards
+        String[] symbols = new String[]{"A", "J", "Q", "K"};
+        for (int i = 0; i < symbols.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                res.add(new Card(symbols[i]));
+            }
+        }
+
+        return res;
     }
 }
--- src/ScanUtils.java
+++ src/ScanUtils.java
@@ -9,18 +9,38 @@ public class ScanUtils {
     private static Scanner sc = new Scanner(System.in);
 
     public static int scanInt(int low, int high) {
-        // TODO:
-        return 0;
+        while (true) {
+            if (sc.hasNext()) {
+                int num = sc.nextInt();
+                if (num < low || num > high) {
+                    System.out.printf("invalid. please input number from %d--%d\n", low, high);
+                    continue;
+                }
+                return num;
+            }
+        }
     }
 
     public static double scanDouble(double low, double high) {
-        // TODO:
-        return 0;
+        while (true) {
+            if (sc.hasNext()) {
+                double num = sc.nextDouble();
+                if (num < low || num > high) {
+                    System.out.printf("invalid. please input number from %f--%f\n", low, high);
+                    continue;
+                }
+                return num;
+            }
+        }
     }
 
     public static String scanString() {
-        // TODO:
-        return "";
+        while (true) {
+            if (sc.hasNext()) {
+                String s = sc.next();
+                return s;
+            }
+        }
     }
 
 }
