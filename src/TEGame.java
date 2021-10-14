81e1e255effc2400a50b850afea90e943c6ba2fd
tongyan <tongyan@haomo.ai>
2021-10-14 18:45:42 -0400

    almost complete TEGame

--- src/TEGame.java
+++ src/TEGame.java
@@ -9,6 +9,7 @@ import java.util.List;
  * @author: Yan Tong, Fangxu Zhou
  **/
 public class TEGame implements Game, CardGame, TurnBasedGame<TEPlayer>, Winnable {
+    public final int INITIAL_DEPOSIT = 1000;
     private TEDealer teDealer;
     private List<TEPlayer> tePlayerList;
     private TEPlayer curPlayer;
@@ -23,10 +24,17 @@ public class TEGame implements Game, CardGame, TurnBasedGame<TEPlayer>, Winnable
         // initialize 1 dealer, num-1 player
         List<TEPlayer> tePlayerList = new ArrayList<>();
         for (int i = 1; i < playerNum; i++) {
-            tePlayerList.add(new TEPlayer(i));
+            TEPlayer newPlayer = new TEPlayer(i);
+            newPlayer.setDeposit(INITIAL_DEPOSIT);
+            newPlayer.getHandCardList().add(new HandCard(0, 31));
+            tePlayerList.add(newPlayer);
         }
         this.tePlayerList = tePlayerList;
+        // init dealer
         this.teDealer = new TEDealer();
+        this.teDealer.setDeposit(3 * INITIAL_DEPOSIT);
+        this.teDealer.setHandCard(new HandCard(0, 31));
+
         this.curPlayerIndex = 0;
         this.curPlayer = this.tePlayerList.get(0);
     }
@@ -55,7 +63,7 @@ public class TEGame implements Game, CardGame, TurnBasedGame<TEPlayer>, Winnable
      */
     @Override
     public void play() {
-        // TODO:
+        welcome();
         while (true) {
             if (hasNextRound()) {
                 nextRound();
@@ -126,12 +134,41 @@ public class TEGame implements Game, CardGame, TurnBasedGame<TEPlayer>, Winnable
     @Override
     public void nextRound() {
         // TODO:
+        dealInitialCard();
+        setPlayerBetting();
+        dealCards2Player(2, true);
         while (this.curPlayer != null) {
-            // TODO: ask player to do action. hit or stand
+            // ask player to do action. hit or stand
+            System.out.printf("Player %d, your hand cards: %s\n",
+                    this.curPlayer.getId(),
+                    playerHandCardInfo(this.curPlayer));
+            System.out.println("enter 'h' for hit, or enter 's' for stand");
+            String choice = ScanUtils.scanString();
+            while (!choice.equalsIgnoreCase("s")) {
+                if (!choice.equalsIgnoreCase("h")) {
+                    System.out.println("invalid input. please enter 'h' for hit, or enter 's' for stand:");
+                    continue;
+                }
+                Card hitCard = this.curPlayer.hit(this.teDealer);
+                System.out.printf("the card you get is %s\n", hitCard.getSymbol());
+                if (this.curPlayer.getHandCardList().get(0).isBusted()) {
+                    System.out.printf("total face value: %d. busted!\n",
+                            this.curPlayer.getHandCardList().get(0).getTotalPoints());
+                    break;
+                }
+                System.out.println("enter 'h' for hit, or enter 's' for stand");
+                choice = ScanUtils.scanString();
+            }
+            System.out.printf("Player %d, your hand cards: %s\n",
+                    this.curPlayer.getId(),
+                    playerHandCardInfo(this.curPlayer));
+            System.out.println("next player.");
+
+            this.curPlayer = nextTurn(this.curPlayer);
         }
-        // TODO: teDealer pick card
-
-
+        // teDealer pick card
+        handleDealer();
+        // at the end of a round, check win
         checkWin();
     }
 
@@ -217,5 +254,98 @@ public class TEGame implements Game, CardGame, TurnBasedGame<TEPlayer>, Winnable
             return true;
         }
         return false;
+    }
+
+    private void dealInitialCard() {
+        // deal card(s) to players and dealer at the beginning of game
+        this.teDealer.getHandCard().addCard(this.teDealer.deal(false));
+        dealCards2Player(1, false);
+    }
+
+    private void dealCards2Player(int cardNum, boolean isPublicCard) {
+        for (TEPlayer tePlayer : this.tePlayerList) {
+            for (int i = 0; i < cardNum; i++) {
+                tePlayer.getHandCardList().get(0).addCard(this.teDealer.deal(isPublicCard));
+            }
+        }
+    }
+
+    /**
+     * @Description: set betting after getting the first card
+     * @Author: Yan Tong
+     */
+    private void setPlayerBetting() {
+        for (TEPlayer tePlayer : this.tePlayerList) {
+            while (true) {
+                System.out.printf("Player%d, your private card is: %s. " +
+                                "Enter 'b' for betting, or 'f' for fold:",
+                        tePlayer.getId(),
+                        tePlayer.getHandCardList().get(0).getHandCardList().get(0).getSymbol());
+                String choice = ScanUtils.scanString();
+                while (!choice.equalsIgnoreCase("b") && !choice.equalsIgnoreCase("f")) {
+                    System.out.println("please enter 'b' for betting, or 'f' for fold:");
+                    choice = ScanUtils.scanString();
+                }
+
+                if (choice.equalsIgnoreCase("b")) {
+                    break;
+                }
+
+                // fold
+                tePlayer.getHandCardList().get(0).removeCard(0);
+                tePlayer.getHandCardList().get(0).addCard(this.teDealer.deal(false));
+                System.out.println("You choose to fold. Change a new card for you.");
+            }
+
+            // betting
+            System.out.printf("Your deposit is: %f, enter the money you want to bet:\n", tePlayer.getDeposit());
+            double betting = ScanUtils.scanDouble(0, tePlayer.getDeposit());
+            while (betting == 0) {
+                System.out.println("Betting has to be larger than 0. Please enter again:");
+                betting = ScanUtils.scanDouble(0, tePlayer.getDeposit());
+            }
+            tePlayer.setBetting(betting);
+            System.out.println("Done");
+        }
+    }
+
+    /**
+     * @Description: get all hand cards' symbols of te player
+     * @Param: player
+     * @return: card symbol string
+     * @Author: Yan Tong
+     */
+    private String playerHandCardInfo(TEPlayer tePlayer) {
+        return printHandCardInfo(tePlayer.getHandCardList().get(0));
+    }
+
+    private String printHandCardInfo(HandCard handCard) {
+        StringBuilder publicSb = new StringBuilder();
+        StringBuilder privateSb = new StringBuilder();
+        publicSb.append("public card(s): ");
+        privateSb.append("private card(s): ");
+        List<Card> cards = handCard.getHandCardList();
+        for (Card card : cards) {
+            if (card.isPublic()) {
+                publicSb.append(card.getSymbol()).
+                        append("(").append(card.getFaceValue()).append(")").
+                        append(" ");
+            } else {
+                privateSb.append(card.getSymbol()).
+                        append("(").append(card.getFaceValue()).append(")").
+                        append(" ");
+            }
+        }
+        publicSb.append("; ").append(privateSb);
+        return publicSb.toString().trim();
+    }
+
+    private void handleDealer() {
+        this.teDealer.revealAllPrivateCard();
+        while (this.teDealer.getHandCard().getTotalPoints() < 27) {
+            this.teDealer.hit();
+        }
+        System.out.println("Dealer cards info:");
+        printHandCardInfo(this.teDealer.getHandCard());
     }
 }
